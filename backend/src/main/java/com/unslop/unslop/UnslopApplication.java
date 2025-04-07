e2334373d4a3cb1b2534d1c659e1c441546c373a
Ayan Ali <ayan_ali2004@hotmail.com>
2025-04-06 23:38:17 -0400

    added spring directory

---
+++ backend/src/main/java/com/unslop/unslop/UnslopApplication.java
@@ -0,0 +1,13 @@
+package com.unslop.unslop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnslopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnslopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/unslop/unslop/UnslopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unslop.unslop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnslopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
